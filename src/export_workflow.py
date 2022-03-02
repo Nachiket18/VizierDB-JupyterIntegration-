ccafb8e22beae9df646bbf98d984946cd3eee058
Nachiket18 <nachideo.em@gmail.com>
2022-03-02 16:19:57 -0500

    Created a new graph structure to handle the issue of multiple dependencies between cells
    This code would be refactored into a better design later.

--- src/export_workflow.py
+++ src/export_workflow.py
@@ -10,23 +10,23 @@ class export_workflow:
     
     def __init__(self,Id,createdAt,action,packageId,commandId,actionModule,modules):
         
-        self.id = Id,
-        self.createdAt = createdAt,
-        self.action = action,
-        self.packageId = packageId,
-        self.commandId = commandId,
-        self.actionModule = actionModule,
+        self.id = Id
+        self.createdAt = createdAt
+        self.action = action
+        self.packageId = packageId
+        self.commandId = commandId
+        self.actionModule = actionModule
         self.modules = modules
     
     def return_workflow(self):
         
         workflow = {}
-        workflow['id'] = self.id,
-        workflow['createdAt'] = self.createdAt,
-        workflow['action'] = self.action,
-        workflow['packageId'] = self.packageId,
-        workflow['commandId'] = self.commandId,
-        workflow['actionModule'] = self.actionModule,
+        workflow['id'] = self.id
+        workflow['createdAt'] = self.createdAt
+        workflow['action'] = self.action
+        workflow['packageId'] = self.packageId
+        workflow['commandId'] = self.commandId
+        workflow['actionModule'] = self.actionModule
         workflow['modules'] = self.modules
         
         return workflow
