83bf238b1d5e072ce78977b6b5780e2426ff85c4
Nachiket18 <nachideo.em@gmail.com>
2022-03-11 16:48:18 -0500

    Modified the code for Gantt Chart Creation
    1. Using broken_barh function to create a gantt chart.
    2. Added the results in png files for serial and parallel execution.

--- src/test_gantt.py
+++ src/test_gantt.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 import pandas as pd
 import sqlite3
+from collections import defaultdict
+import matplotlib.patches as mpatches
 
 con = sqlite3.connect('/home/nachiket/vizier-scala/vizier.db/Vizier.db')
 
@@ -11,41 +13,45 @@ cur = con.cursor()
 # for row in cur.execute(' SELECT c.module_id, c.state, c.created, r.started, r.finished FROM cell c LEFT OUTER JOIN result r ON c.result_id = r.id WHERE c.workflow_id = (SELECT max(id) FROM workflow);'):
 #     print(row)
 
-df = pd.read_sql("SELECT c.module_id, c.state, strftime('%H%M%S',time(c.created/10, 'unixepoch')) as created, strftime('%H%M%S',time(r.started/10,'unixepoch')) as started, strftime('%H%M%S',time(r.finished/10,'unixepoch')) as finished FROM cell c LEFT OUTER JOIN result r ON c.result_id = r.id WHERE c.workflow_id = (SELECT max(id) FROM workflow)", con)
+df = pd.read_sql("SELECT c.module_id, c.state, strftime('%H%M%S',time(c.created/10, 'unixepoch')) as created, strftime('%H%M%S',time(r.started/10,'unixepoch')) as started, strftime('%H%M%S',time(r.finished/10,'unixepoch')) as finished FROM cell c LEFT OUTER JOIN result r ON c.result_id = r.id WHERE c.workflow_id = 30", con)
+
+
+df['created'] = df['created'].astype('int')
+df['started'] = df['started'].astype('int')
+df['finished'] = df['finished'].astype('int')
+
 
 print(df)
 
-bars_1 = df['created'].astype('int')
-bars_2 = df['started'].astype('int')
-bars_3 = df['started'].astype('int')
 
-print(bars_1)
-print(bars_2)
-print(bars_3)
+fig, gnt = plt.subplots()
 
-bars = np.add(bars_1, bars_2).tolist()
-
-print(bars)
+colors_dict = defaultdict( CreatetoStart ='Red', StarttoFinish ='Yellow')
 
 
-r = [i for i in range(0,len(bars_1))]
+gnt.set_xlabel('Time since Notebook Execution')
+gnt.set_ylabel('Modules')
 
-names = [module for module in df['module_id']]
-barWidth = 1
-
-# Create brown bars
-plt.bar(r, bars_1, color='green', edgecolor='white', width=barWidth)
-# Create green bars (middle), on top of the first ones
-plt.bar(r, bars_2, bottom=bars_1, color='orange', edgecolor='white', width=barWidth)
-# Create green bars (top)
-plt.bar(r, bars_3, bottom=bars, color='blue', edgecolor='white', width=barWidth)
+gnt.grid(True)
 
 
-plt.xticks(r, names, fontweight='bold')
-plt.xlabel("Modules")
+#gnt.broken_barh([(83615,23),(83638,862)],(10, 9), facecolors = ('red','yellow'))
 
-plt.savefig('Task_chart_Parallel')
+# Setting X-axis limits
+gnt.set_xlim((df['created'].iloc[0] - 100), 45000)
 
-#print(df['created'])
+gnt.set_ylim(0, 320)
+y_ticks = [i for i in range(0,df['created'].count())]
+gnt.set_yticklabels(y_ticks)
+
+i = 0
+for label, content in df.iterrows():
+    print(content['created'],content['started'])
+    gnt.broken_barh([(content['created'],(content['started'] - content['created'])),(content['started'], (content['finished'] - content['started'])) ],( i * 30, 9), facecolors =('red','yellow'))
+    i += 1 
+
+patches = [mpatches.Patch(color=color, label=key) for (key, color) in colors_dict.items()]
+plt.legend(handles=patches,loc='upper right')
+plt.savefig("task_chart_serial.png")
 
 
